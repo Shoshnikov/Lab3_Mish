9cef257265265196bf5e025e0bd81f8b3003f54f
Ivan Shoshnikov <shoshnikov.ivan@mail.ru>
2019-10-21 00:59:02 +0300

    First commit

---
+++ Lab3_Mish/Lab3_Mish.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int CorrectInput(int& value);
+
+int main()
+{
+	setlocale(LC_ALL,"RUS");
+	int matr[100][50];
+	int arr[50];
+	int n, m, k;
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите количество строк и столбцов через пробел\n";
+	cin >> n >> m;
+	cout << "Вводите числа...\n";
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			CorrectInput(matr[i][j]);
+	cout << "Введите чсило k\n";
+	cin >> k;
+	for (int i = 0; i < n; i++)
+		arr[i] = 0;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			if (matr[i][j] % k == 0)
+				arr[i] += matr[i][j];
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << "\t";
+	cout << endl;
+}
+
+int CorrectInput(int& value)
+{
+	char fail;
+	cin >> value;
+	while (cin.fail())
+	{
+		cout << "Данные некорректны\n";
+		cin.clear();
+		cin >> fail;
+		cin >> value;
+	}
+	return value;
+}
